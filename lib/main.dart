bcd7261a87d762d803fe38072e7928fdf703b352
ADIO <royjosphp@gmail.com>
2022-01-05 11:48:13 +0530

    trail1

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner:false,
       routes: {
         '/ScreenRegUser':(context) =>const ScreenRegUser(),
-       
+        
       },
 home: const MyHome(
   
